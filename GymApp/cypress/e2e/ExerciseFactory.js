6d8c8f7534ae8ade38e1481c06402e54d9d1b9d6
Giorgi Mazmishvili <giorgi.mazmishvili@telefonica.com>
2023-01-23 19:56:05 +0100

    Validation E2E Test refactor

---
+++ GymApp/cypress/e2e/ExerciseFactory.js
@@ -0,0 +1,24 @@
+export default {
+  create: function (name, muscle, difficulty, description) {
+    cy.get("[data-test='create-button']").click();
+    if (name) cy.get("[data-test='exercise-name']").find("input").type(name);
+    if (muscle) cy.get("[data-test='muscle-group']").find("input").type(muscle);
+    cy.get("[data-test='difficulty']").click();
+    if (difficulty)
+      cy.get(".v-overlay-container").within(() => {
+        cy.get("div").contains(difficulty).click();
+      });
+    if (description)
+      cy.get("[data-test='description']").find("input").type(description);
+    cy.get("[data-test='save-button']").click();
+  },
+  delete: function (name) {
+    cy.get(".catalogCardName")
+      .contains(name)
+      .parent()
+      .parent()
+      .within(() => {
+        cy.get(".catalogCardDelete").click();
+      });
+  },
+};
--- GymApp/cypress/e2e/validationTest.cy.js
+++ GymApp/cypress/e2e/validationTest.cy.js
@@ -1,77 +1,34 @@
+import exerciseFactory from "./ExerciseFactory";
+
 describe("Can't create a new exercise if", () => {
   beforeEach(() => {
     cy.visit("catalog");
   });
 
   it("Name is empty", () => {
-    cy.get("[data-test='create-button']").click();
-    cy.get("[data-test='muscle-group']").find("input").type("Chest");
-
-    cy.get("[data-test='difficulty']").click();
-    cy.get(".v-overlay-container").within(() => {
-      cy.get("div").contains("Medium").click();
-    });
-
-    cy.get("[data-test='description']").find("input").type("Do it correct!");
-
-    cy.get("[data-test='save-button']").click();
-
-    cy.get(".v-messages__message")
-      .first()
-      .should("have.text", "Field is required!");
+    exerciseFactory.create("", "chest777", "Hard", "Do bench press");
+    cy.get(".all-exercises").should("not.contain.text", "chest777");
+    cy.get(".v-messages__message").should("have.text", "Field is required!");
   });
 
   it("Muscle is empty", () => {
     const exerciseName = "Chest4464464";
-    cy.get("[data-test='create-button']").click();
-    cy.get("[data-test='exercise-name']").find("input").type(exerciseName);
-
-    cy.get("[data-test='difficulty']").click();
-    cy.get(".v-overlay-container").within(() => {
-      cy.get("div").contains("Medium").click();
-    });
-
-    cy.get("[data-test='description']").find("input").type("Do it correct!");
-
-    cy.get("[data-test='save-button']").click();
-
-    cy.get(".v-messages__message")
-      .first()
-      .should("have.text", "Field is required!");
-
+    exerciseFactory.create(exerciseName, "", "Hard", "Do bench press");
+    cy.get(".v-messages__message").should("have.text", "Field is required!");
     cy.get(".all-exercises").should("not.contain.text", exerciseName);
   });
 
   it("Difficulty is empty", () => {
     const exerciseName = "Chest446146464";
-    cy.get("[data-test='create-button']").click();
-    cy.get("[data-test='exercise-name']").find("input").type(exerciseName);
-    cy.get("[data-test='muscle-group']").find("input").type("Chest");
-    cy.get("[data-test='description']").find("input").type("Do it correct!");
-
-    cy.get("[data-test='save-button']").click();
-
+    exerciseFactory.create(exerciseName, "chest", "", "Do bench press");
     cy.get(".v-messages__message").should("have.text", "Field is required!");
     cy.get(".all-exercises").should("not.contain.text", exerciseName);
   });
 
   it("Description is empty", () => {
     const exerciseName = "Chest446436464";
-    cy.get("[data-test='create-button']").click();
-    cy.get("[data-test='exercise-name']").find("input").type(exerciseName);
-    cy.get("[data-test='muscle-group']").find("input").type("Chest");
-
-    cy.get("[data-test='difficulty']").click();
-    cy.get(".v-overlay-container").within(() => {
-      cy.get("div").contains("Medium").click();
-    });
-
-    cy.get("[data-test='save-button']").click();
-
-    cy.get(".v-messages__message")
-      .first()
-      .should("have.text", "Field is required!");
-
+    exerciseFactory.create(exerciseName, "chest", "Easy", "");
+    cy.get(".v-messages__message").should("have.text", "Field is required!");
     cy.get(".all-exercises").should("not.contain.text", exerciseName);
   });
 });
@@ -82,22 +39,16 @@ describe("Can create a new exercise if", () => {
   });
 
   it("All field are correct", () => {
-    const exerciseName = "Chest474614";
-    cy.get("[data-test='create-button']").click();
-    cy.get("[data-test='exercise-name']").find("input").type(exerciseName);
-    cy.get("[data-test='muscle-group']").find("input").type("Chest");
-
-    cy.get("[data-test='difficulty']").click();
-    cy.get(".v-overlay-container").within(() => {
-      cy.get("div").contains("Medium").click();
-    });
-
-    cy.get("[data-test='description']").find("input").type("Do it correct!");
-
-    cy.get("[data-test='save-button']").click();
-
+    const exerciseName = "Biceps Curls";
+    exerciseFactory.create(
+      exerciseName,
+      "Biceps",
+      "Easy",
+      "Do some curls for ur biceps"
+    );
     cy.get("div").should("not.contain.text", "Field is required!");
-
     cy.get(".all-exercises").should("contain.text", exerciseName);
+
+    exerciseFactory.delete(exerciseName);
   });
 });
