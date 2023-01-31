600ce20dc01dcfefc6867a683590795d4c7f3859
GiorgiMazm <booster1637@mail.ru>
2023-01-31 15:51:37 +0100

    Start with frontend for Training day

--- GymApp/src/components/GymRecordsPage.vue
+++ GymApp/src/components/GymRecordsPage.vue
@@ -14,6 +14,47 @@
               :dayIndex="index"
             ></TrainingDay>
           </div>
+
+          <button
+            v-if="!createNew"
+            @click="createNewToggle"
+            data-test="create-button"
+          >
+            Create new training day
+          </button>
+
+          <Form
+            name="newTrainingDayForm"
+            data-test="new-training-day-form"
+            v-if="createNew"
+          >
+            <Field name="muscleGroup" v-slot="{ field, errors }">
+              <v-text-field
+                data-test="training-day-type"
+                v-bind="field"
+                class="w-25"
+                name="trainingType"
+                label="Training Type"
+                v-model="trainingType"
+                :error-messages="errors"
+              ></v-text-field>
+            </Field>
+
+             <button class="d-block">Add exercise</button>
+
+            <button type="submit" data-test="save-button">Save</button>
+
+            <button
+              class="ma-2"
+              @click="
+                createNewToggle();
+                clearCreateInputs();
+              "
+              data-test="cancel-button"
+            >
+              Cancel
+            </button>
+          </Form>
         </v-main>
       </v-layout>
     </v-card>
@@ -25,13 +66,38 @@ import AppHeader from "./AppHeader.vue";
 import TrainingDay from "./TrainingDay.vue";
 import { useGymStore } from "@/stores/GymStore";
 import { mapState } from "pinia";
+import { Field, Form } from "vee-validate";
 
 export default {
   name: "GymRecords",
   components: {
     AppHeader,
+    Form,
+    Field,
     TrainingDay,
   },
+  data() {
+    return {
+      chosenFilter: false,
+      trainingType: "",
+      createNew: false,
+      exerciseArray: [{ name: "Bench", sets: [{ weight: 10, repetition: 7 }] }],
+
+      exerciseName: "",
+      setWeight: 0,
+      setRepetition: 0
+    };
+  },
+
+  methods: {
+    createNewToggle() {
+      this.createNew = !this.createNew;
+    },
+    clearCreateInputs() {
+      this.trainingType = "";
+    },
+  },
+
   computed: {
     ...mapState(useGymStore, ["trainingDays"]),
   },
